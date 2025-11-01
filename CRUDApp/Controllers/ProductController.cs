e696a4d36cad4766a2a9b8f6328456e6994affe6
Admin <Admin@DESKTOP-QH3U7QV>
2025-11-02 01:50:00 +0530

    Add project files.

---
+++ CRUDApp/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using CRUDApp.Data;
+using CRUDApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CRUDApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Name, Price, Description, Stock")]Product model)
+        {
+            if (ModelState.IsValid) { 
+                _context.Products.Add(model);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Created Successfully!!";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit([Bind("Id, Name, Price, Description, Stock")] Product model) 
+        {
+            if (ModelState.IsValid) { 
+                _context.Products.Update(model);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Updated Successfully!!";
+                TempData["NotificationType"] = "success";
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        
+        public IActionResult Delete(int id) 
+        {
+            var products = _context.Products.Find(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(products);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var products = _context.Products.Find(id);
+            if(products != null)
+            {
+                _context.Products.Remove(products);
+                _context.SaveChanges();
+                TempData["Notification"] = "Product Deleted Successfully!!";
+                TempData["NotificationType"] = "success";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
